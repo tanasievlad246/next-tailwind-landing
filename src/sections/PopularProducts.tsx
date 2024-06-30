7de1f8658f3628615daeb70e4efbd30feff4158c
vladtanasiedns <tanasie.vlad@dandsltd.com>
2024-06-30 12:56:19 +0300

    Working on PopularProducts

--- src/sections/PopularProducts.tsx
+++ src/sections/PopularProducts.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
+import { products } from '@/constants'
+
 const PopularProducts = () => {
   return (
-    <div>PopularProducts</div>
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-palanquin font-bold">Popular Products</h2>
+        <p>Experience to notch quality and style with our saught after collections!</p>
+      </div>
+    </section>
   )
 }
 
